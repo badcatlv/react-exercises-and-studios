f5a7d1c27808ab6bc787fc3bebc1b30a372d09e0
Colleen Blake <colleenemilyblake@gmail.com>
2023-07-10 08:26:01 -0500

    Added hobby URLS

---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,9 @@
+export default function HobbyLinks(){
+    const hobbyLinks = ["https://www.springfieldleather.com/", "https://www.sheldonbrown.com/"];
+    return (
+        <div>
+            <a href= {hobbyLinks[0]}>Leather Work</a>
+            <a href= {hobbyLinks[1]}>Bicycles</a>
+        </div>
+    )
+}
