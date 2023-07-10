49b8a28c59eb0b5ac9de42956d840206e733e98b
Colleen Blake <colleenemilyblake@gmail.com>
2023-07-10 08:52:07 -0500

    Finished Page

--- part1/exercises/src/components/HobbyLinks.js
+++ part1/exercises/src/components/HobbyLinks.js
@@ -2,7 +2,10 @@ export default function HobbyLinks(){
     const hobbyLinks = ["https://www.springfieldleather.com/", "https://www.sheldonbrown.com/"];
     return (
         <div>
-            <a href= {hobbyLinks[0]}>Leather Work</a>
+            <h3 style={{color: "rgb(46, 158, 147)"}}>Hobbies</h3>
+            <br />
+            <a href= {hobbyLinks[0]}>Leather Work<br /></a>
+            <br />
             <a href= {hobbyLinks[1]}>Bicycles</a>
         </div>
     )
