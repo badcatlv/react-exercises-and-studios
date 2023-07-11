d9d0e1e2ef98bd129409c66cf52050e9e58f6fca
Colleen Blake <colleenemilyblake@gmail.com>
2023-07-11 15:26:44 -0500

    Now to add the recipe

---
+++ part1/studio/src/components/Photos.js
@@ -0,0 +1,7 @@
+import styles from './Description.module.css';
+
+export default function RecipePhoto(){
+    return(
+        <img src="valid URL" alt="recipe" className={styles.imageUpdates} />
+    )
+}
