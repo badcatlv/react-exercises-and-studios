b0def48d4efa2b35a24fb17dd660792a048da099
Colleen Blake <colleenemilyblake@gmail.com>
2023-07-12 19:41:15 -0500

    play around and finish

--- part1/studio/src/components/Photos.js
+++ part1/studio/src/components/Photos.js
@@ -2,6 +2,6 @@ import styles from './Description.module.css';
 
 export default function RecipePhoto(){
     return(
-        <img src="https://www.allrecipes.com/thmb/HvFNHlXBERKoqEmy3Vxcfu9BJ_0=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/238423-dads-creamy-cucumber-salad-DDMFS-4x3-36063371e89e463d91c5282ae0873779.jpg" alt="recipe" className={styles.imageUpdates} />
+        <img className="imageUpdates" src="https://www.allrecipes.com/thmb/HvFNHlXBERKoqEmy3Vxcfu9BJ_0=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/238423-dads-creamy-cucumber-salad-DDMFS-4x3-36063371e89e463d91c5282ae0873779.jpg" alt="recipe" className={styles.imageUpdates} />
     )
 }
